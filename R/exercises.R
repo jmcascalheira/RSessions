cd1a53eebdc8e9845e8cec89b53d8810e44385f6
João <jmcasca@gmail.com>
2018-11-24 18:29:04 +0000

    Ex 1 added

---
+++ R/exercises.R
@@ -0,0 +1,12 @@
+
+
+# To complete the exercises below use the dataset.csv file available in \data
+
+# Using the tidyverse framework and functions from the dplyr package write lines of code to perform the following actions:
+#
+#  1- calculate the mean and standard-deviation of all numerical variables in the database
+#  2- calculate the mean and standar-deviation of two new variables: elongation (Lenght*Width) and flattening (Width*Thickness)
+#  3- create a new data.frame with only the artifacts that have faceted platforms
+#  4- calculate the number and percentage of the different categories of cortex_percentage for all flakes with faceted platforms
+#  5- create a cross-table with the variables Shape and Profile
+#
